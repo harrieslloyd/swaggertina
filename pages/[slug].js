10aa9c3f1474eb46a2853841042dec6d37be1d82
Harries Lloyd <harrieslloyd9@gmail.com>
2024-02-07 00:45:54 -0700

    fine

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,7 +2,6 @@ import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { Layout } from "../components/Layout.js";
 import { tinaField, useTina } from "tinacms/dist/react";
 import { client } from "../tina/__generated__/client";
-import { Header } from "../components/Header.js";
 
 export default function Home(props) {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
@@ -16,14 +15,26 @@ export default function Home(props) {
   const content = data.page.body;
   return (
     <Layout>
-      <Header pages={pages} />
+      <header>
+        <table>
+          <tbody>
+            <tr>
+              {
+                pages.map((page) => {
+                  return <th className="navcol"><a href={"/" + page.node._sys.filename}>{page.node.title}</a></th>
+                })
+              }
+            </tr>
+          </tbody>
+        </table>
+      </header>
       <section className="titlesection">
-      <h1 data-tina-field={tinaField(data.page, "title")} >{data.page.title}</h1>
+        <h1 data-tina-field={tinaField(data.page, "title")} >{data.page.title}</h1>
       </section>
       <section className="mainsection">
-      <div data-tina-field={tinaField(data.page, "body")}>
-        <TinaMarkdown content={content} />
-      </div>
+        <div data-tina-field={tinaField(data.page, "body")}>
+          <TinaMarkdown content={content} />
+        </div>
       </section>
     </Layout>
   );
