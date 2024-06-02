483aee493952d6d500a2ee3615ba85ab38730b7a
Harries Lloyd <harrieslloyd9@gmail.com>
2024-06-02 12:04:26 -0600

    vercel analytics?!

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,19 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { Analytics } from "@vercel/analytics/react"
 
 export const Layout = (props) => {
   
   return (
     <>
+    <Analytics>
       <Head>
         <title>{props.title}</title>
         <meta name="description" content="A TinaCMS Application" />
         <link rel="icon" href={props.fav} />
       </Head>
       <main>{props.children}</main>
+      </Analytics>
     </>
   )
 }
