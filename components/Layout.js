341c8c75785ea8ee3ca6d615e139f232f3016f19
Harries Lloyd <harrieslloyd9@gmail.com>
2024-06-02 12:06:34 -0600

    ok whoops

--- components/Layout.js
+++ components/Layout.js
@@ -6,14 +6,12 @@ export const Layout = (props) => {
   
   return (
     <>
-    <Analytics>
       <Head>
         <title>{props.title}</title>
         <meta name="description" content="A TinaCMS Application" />
         <link rel="icon" href={props.fav} />
       </Head>
-      <main>{props.children}</main>
-      </Analytics>
+      <main>{props.children}<Analytics /></main>
     </>
   )
 }
