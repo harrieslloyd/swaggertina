762bc40cf67a1d7b49b67e79bad49c70be25e590
Harries Lloyd <harrieslloyd9@gmail.com>
2024-06-02 12:03:09 -0600

    forgot key

--- components/horiz.js
+++ components/horiz.js
@@ -18,7 +18,7 @@ export const Horiz = (props) => {
                 { 
                     props.fields.map((field, index) => {
                             return (
-                                <div>
+                                <div key={index}>
                                     <img href={(field.img)} alt={field.label} />
                                     <h2>{field.label}</h2>
                                     <TinaMarkdown content={field.text} />
